147a014e8bf48f789f2dc2c88e571782205d032d
wangzheng <wangzheng@guahao.com>
2017-09-22 16:49:48 +0800

    store scrapy result by scrapy.Item

--- com/zwang/scrapycmd/scrapycmd/spiders/dmoz_spider.py
+++ com/zwang/scrapycmd/scrapycmd/spiders/dmoz_spider.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import scrapy
 
+from scrapycmd.items import ScrapycmdItem
+
+
 class DmozSpider(scrapy.Spider):
-    name = "dmoz" #命令行运行：scrapy crawl dmoz
+    name = "dmoz"  # 命令行运行：scrapy crawl dmoz
     allowed_domains = ["dmoz.org"]
     start_urls = [
         # "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
@@ -11,6 +16,15 @@ class DmozSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        # filename = response.url.split("/")[-2] + '.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+
+
+        logging.warning("This is a warning")
+        for sel in response.xpath('//ul/li'):
+            scrapycmdItem = ScrapycmdItem();
+            scrapycmdItem['title'] = sel.xpath('a/text()').extract()  # 打印的是unicode
+            scrapycmdItem['link'] = sel.xpath('a/@href').extract()
+            scrapycmdItem['desc'] = sel.xpath('text()').extract()
+            yield scrapycmdItem
